4055e196168e57964ced3fd5e7f8d028395d5b23
Varenchinusu Michael <michael.hansetyanto@kurisutaru.net>
2025-10-15 13:09:07 +0700

    Logs : Adding subfolder for all log zip (1)
    Forgot to check directory exist

--- couponstance.py
+++ couponstance.py
@@ -78,7 +78,11 @@ def kuri_zip_compression(file_path):
     directory = os.path.dirname(file_path) or "."
     today = datetime.now().strftime("%Y-%m-%d")
 
-    zip_filename = os.path.join(directory, 'logs', f"app.{today}.log.zip")
+    log_dir = os.path.join(directory, 'logs')
+    if not os.path.exists(log_dir):
+        os.makedirs(log_dir)
+
+    zip_filename = os.path.join(log_dir, f"app.{today}.log.zip")
 
     with zipfile.ZipFile(zip_filename, 'w', zipfile.ZIP_DEFLATED) as zf:
         zf.write(file_path, f"app.{today}.log")
